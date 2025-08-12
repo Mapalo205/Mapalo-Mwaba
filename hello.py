1256f9da4cd9965f5d2ef92bc3db5bb91e2b2d58
Mapalo205 <mwabamapalo6@gmail.com>
2025-08-12 20:14:26 +0200

    Task 1

---
+++ hello.py
@@ -0,0 +1,5 @@
+name = input ('what is your name')
+birth_year= int(input('in which year whare you born'))
+current_year = 2025
+age = birth_year -current_year
+print(f'{name} ,your are {age} years old ')
